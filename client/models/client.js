34e0007a57942584993080e08d33d9e5e7030e03
fneiszjoco <fneisz@freemail.hu>
2012-05-11 11:42:55 -0700

    commit

---
+++ client/models/client.js
@@ -0,0 +1,12 @@
+Ext.define('designer.model.client', { 
+ 	 extend: 'designer.model.CRBaseObject',
+ 	 alias: 'client'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
