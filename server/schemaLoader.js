86673234c9ad20a41c7930721e9c35879a2625b4
fneiszjoco <fneisz@freemail.hu>
2012-06-11 08:55:08 -0700

    commit

---
+++ server/schemaLoader.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+
+exports.MySchema = require('../schemas/MySchema').MySchema;
+exports.Comment = require('../schemas/Comment').Comment;
+exports.User = require('../schemas/User').User;
+exports.BlogPost2 = require('../schemas/BlogPost2').BlogPost2;
+exports.persistent = require('../schemas/persistent').persistent;
+exports.Mytree = require('../schemas/Mytree').Mytree;
