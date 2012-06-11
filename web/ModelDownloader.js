acd6053792c72988a5e327ec727b914f8d08ba9a
fneiszjoco <fneisz@freemail.hu>
2012-06-11 15:21:16 -0700

    commit

--- web/ModelDownloader.js
+++ web/ModelDownloader.js
@@ -2,7 +2,7 @@ Ext.define('web.ModelDownloader', {
  	 Download: function() {
  	 	 Ext.syncRequire('web.model.CRBaseObject');
  	 	 Ext.syncRequire('web.model.PersistentMaster');
- 	 	 Ext.syncRequire('web.model.PPtree');
+ 	 	 Ext.syncRequire('web.model.PMtree');
  	 }
  });
  
