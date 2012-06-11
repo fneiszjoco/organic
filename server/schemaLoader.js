57852cf1f59b1cadb98844ccb2bc36f3b083395c
fneiszjoco <fneisz@freemail.hu>
2012-06-11 15:21:19 -0700

    commit

--- server/schemaLoader.js
+++ server/schemaLoader.js
@@ -2,4 +2,4 @@ var mongoose = require('mongoose');
 
 exports.CRBaseObject = require('../schemas/CRBaseObject').CRBaseObject;
 exports.PersistentMaster = require('../schemas/PersistentMaster').PersistentMaster;
-exports.PPtree = require('../schemas/PPtree').PPtree;
+exports.PMtree = require('../schemas/PMtree').PMtree;
