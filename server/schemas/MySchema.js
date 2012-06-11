2bfffd6e2794acdd085f61bfe078c33bfdf7bcf2
fneiszjoco <fneisz@freemail.hu>
2012-06-11 14:33:37 -0700

    commit

--- server/schemas/MySchema.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.MySchema = new mongoose.Schema({ 
-		_id: { type: String }
-});
-
-exports.MySchema.plugin(commonPlugin);
--- server/schemas/User.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.User = new mongoose.Schema({ 
-		_id: { type: String }
-		,name: { type: String }
-});
-
-exports.User.plugin(commonPlugin);
