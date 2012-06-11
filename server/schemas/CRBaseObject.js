14ffb94b59473069a5b2eaceaf636c22b5ca8806
fneiszjoco <fneisz@freemail.hu>
2012-06-11 15:21:17 -0700

    commit

--- server/schemas/CRBaseObject.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.CRBaseObject = new mongoose.Schema({ 
-		_id: { type: String }
-		,_type: { type: String }
-});
-
-exports.CRBaseObject.plugin(commonPlugin);
